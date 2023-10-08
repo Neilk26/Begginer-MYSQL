1d0e15849063226ed5f546bc39e6981217db2a4f
Neilk26 <146628310+Neilk26@users.noreply.github.com>
2023-10-08 12:54:16 +0100

    Update How_to_search_for_words_containing.sql
    how to search for specific phrases in rows of data in this case the word cars in company's names that contains the word "cars".

--- How_to_search_for_words_containing.sql
+++ How_to_search_for_words_containing.sql
@@ -1,2 +1,4 @@
-select * from customers 
-where CustomerName like "%toys%";                             # THE LIKE OPERATOR IS USED WHEN TRYING TO FIND A WORD OR PHRASE IN WHOLE WORDS AND USE THE % AT THE BEGGING AND END OF THE PHRASE OR WORD IN QUESTION.
+select  distinct *                             # the distinct operator makes it so they only list the same result once instead of every instance.
+from productlines
+where productline like "%cars%";               # when searching for rows of data that contain a certain word use LIKE instead of = and use % at both sides of the word to mean containing"%cars%".
+											                         # If the % it as the front it means begin with if it is at the end it means ending with "%cars" and "cars %".
